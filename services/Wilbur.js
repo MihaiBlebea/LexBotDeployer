f7a3d1d73346fa444c4db59285a58c61e9424c7c
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-09-19 14:14:43 +0100

    test

--- services/Wilbur.js
+++ services/Wilbur.js
@@ -2,24 +2,44 @@
 
 class Wilbur
 {
-    constructor(requests)
+    constructor(requests, files)
     {
         this.requests = requests
+        this.files = files
+
         this.slotVersions = []
+        this.intentVersions = []
     }
 
-    deploySlots(files)
+    deploySlots()
     {
         const slotFolder = './../repos/slots'
-        files.processFiles(slotFolder, (files)=> {
+        this.files.processFiles(slotFolder, (slotFiles)=> {
 
-            files.map((file)=> {
+            slotFiles.map((file)=> {
                 this.requests.deploySlotRequest(file, (data)=> {
                     this.slotVersions.push({
                         name: data.name,
                         version: data.version
                     })
                     console.log(this.slotVersions)
+                })
+            })
+        })
+    }
+
+    deployIntents()
+    {
+        const slotFolder = './../repos/intents'
+        this.files.processFiles(slotFolder, (intentFiles)=> {
+
+            intentFiles.map((file)=> {
+                this.requests.deployIntentRequest(file, (data)=> {
+                    this.intentVersions.push({
+                        name: data.name,
+                        version: data.version
+                    })
+                    console.log(this.intentVersions)
                 })
             })
         })
