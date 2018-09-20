1e1bffab9b3fa137825abe0450c8edbb154a9b2b
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-09-20 09:42:02 +0100

    test

--- services/Wilbur.js
+++ services/Wilbur.js
@@ -73,7 +73,7 @@ class Wilbur
                         name: data.name,
                         version: data.version
                     })
-                })
+                }, 2000)
             })
         })
     }
