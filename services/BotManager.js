ba4320cbd0cc054600b0a9f50c385dece51bf738
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-09-25 13:56:39 +0100

    test

--- services/BotManager.js
+++ services/BotManager.js
@@ -1,3 +1,5 @@
+const { exec } = require('child_process')
+
 
 class BotManager
 {
@@ -50,44 +52,115 @@ class BotManager
 
     createBotVersion(botName, checksum, callback)
     {
-        var params = {
-            name: botName,
-            checksum: checksum
-        }
-
-        this.lex.createBotVersion(params, (error, data)=> {
-            if(error)
-            {
-                console.log(error.stack);
-            } else {
-                this.log(`Creating a new version for bot ${data.name}. New bot version is ${data.version}`)
-                if(callback)
-                {
-                    callback(data)
-                }
-            }
-        })
-    }
-
-    getBotVersions(botName, callback)
-    {
-        var params = {
-            name: botName,
-            versionOrAlias: '$LATEST'
-        }
-        this.lex.getBot(params, (error, data)=> {
-            // if(error) console.log(error, error.stack)
-            if(data !== null)
-            {
-                this.log(`Getting the checksum for the latest version of bot ${data.name}.`)
-            }
-
+        exec(`aws lex-models create-bot-version \
+                    --region us-east-1 \
+                    --name ${botName} \
+                    --checksum "${checksum}"`, (error, stdout, stderr)=> {
+            if(error) console.log(error)
+            console.log(stdout)
+            console.log(stderr)
+            var data = JSON.parse(stdout)
+            this.log(`Creating a new version for ${data.name}. New version is ${data.version}`)
             if(callback)
             {
                 callback(data)
             }
         })
+
+        // var params = {
+        //     name: botName,
+        //     checksum: checksum
+        // }
+        //
+        // this.lex.createBotVersion(params, (error, data)=> {
+        //     if(error)
+        //     {
+        //         console.log(error.stack);
+        //     } else {
+        //         this.log(`Creating a new version for bot ${data.name}. New bot version is ${data.version}`)
+        //         if(callback)
+        //         {
+        //             callback(data)
+        //         }
+        //     }
+        // })
     }
+
+    // createBotVersion(botName, checksum, callback)
+    // {
+    //     var params = {
+    //         name: botName,
+    //         checksum: checksum
+    //     }
+    //
+    //     this.lex.createBotVersion(params, (error, data)=> {
+    //         if(error)
+    //         {
+    //             console.log(error.stack);
+    //         } else {
+    //             this.log(`Creating a new version for bot ${data.name}. New bot version is ${data.version}`)
+    //             if(callback)
+    //             {
+    //                 callback(data)
+    //             }
+    //         }
+    //     })
+    // }
+
+    getBotVersions(botName, callback)
+    {
+        exec(`aws lex-models get-bot \
+                    --region us-east-1 \
+                    --name ${botName} \
+                    --version-or-alias "\\$LATEST"`, (error, stdout, stderr)=> {
+            if(error) console.log(error)
+            console.log(stdout)
+            console.log(stderr)
+            var data = JSON.parse(stdout)
+            this.log(`Creating a new version for ${data.name}. New version is ${data.version}`)
+            if(callback)
+            {
+                callback(data)
+            }
+        })
+
+        // var params = {
+        //     name: botName,
+        //     versionOrAlias: '$LATEST'
+        // }
+        // this.lex.getBot(params, (error, data)=> {
+        //     // if(error) console.log(error, error.stack)
+        //     if(data !== null)
+        //     {
+        //         this.log(`Getting the checksum for the latest version of bot ${data.name}.`)
+        //     }
+        //
+        //     if(callback)
+        //     {
+        //         callback(data)
+        //     }
+        // })
+    }
+
+    // getBotVersions(botName, callback)
+    // {
+    //     var params = {
+    //         name: botName,
+    //         versionOrAlias: '$LATEST'
+    //     }
+    //     this.lex.getBot(params, (error, data)=> {
+    //         // if(error) console.log(error, error.stack)
+    //         if(data !== null)
+    //         {
+    //             this.log(`Getting the checksum for the latest version of bot ${data.name}.`)
+    //         }
+    //
+    //         if(callback)
+    //         {
+    //             callback(data)
+    //         }
+    //     })
+    // }
 
     deleteBot(botName)
     {
@@ -100,28 +173,6 @@ class BotManager
                 console.log(error.stack);
             } else {
                 console.log(data)
-            }
-        })
-    }
-
-    putBotAlias(params, callback)
-    {
-        // var params = {
-        //     botName: 'STRING_VALUE',
-        //     botVersion: 'STRING_VALUE',
-        //     name: 'STRING_VALUE',
-        //     checksum: 'STRING_VALUE',
-        //     description: 'STRING_VALUE'
-        // };
-        this.lex.putBotAlias(params, (error, data)=> {
-            if(error)
-            {
-                console.log(error, error.stack);
-            } else {
-                if(callback)
-                {
-                    callback(data)
-                }
             }
         })
     }
@@ -146,7 +197,7 @@ class BotManager
                                 botVersion: data.version,
                                 name: 'Testing'
                             }
-                            
+
                             if(callback)
                             {
                                 setTimeout(()=> {
