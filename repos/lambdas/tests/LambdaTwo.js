e4dd0bd38d3dfe5b118d805a13aa3f092678f79e
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-09-27 09:40:16 +0100

    test

--- repos/lambdas/tests/LambdaTwo.js
+++ repos/lambdas/tests/LambdaTwo.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const lambda = require('./../functions/LambdaTwo.js')
+const lambda = require('./../functions/LambdaTwo/LambdaTwo.js')
 
 
 describe('Test lambda function', function() {
