d6c7342608e2b01e3f53542622028f3a288017fe
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-10-05 15:05:41 +0100

    test

--- repos/lambdas/tests/LambdaOne.js
+++ repos/lambdas/tests/LambdaOne.js
@@ -4,6 +4,6 @@ const lambda = require('./../functions/LambdaOne/LambdaOne.js')
 
 describe('Test lambda function', function() {
     it('Should return hello World', function() {
-        assert.equal(lambda.hello(), 'hello World')
+        assert.equal(lambda.hello(), 'hello World222')
     })
 })
